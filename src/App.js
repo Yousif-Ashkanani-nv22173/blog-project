41ab7735f2a9f5c795215e5b716819a61701a89f
Yousif-Ashkanani-nv22173 <149366570+Yousif-Ashkanani-nv22173@users.noreply.github.com>
2024-02-15 06:53:09 +0000

    student card

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from "./components/Card"
+function App() {
+  return (
+    <div className="App">
+      <header className='app-header gap-y-4'>
+      <Card/>
+     <Card/>
+     <Card/>
+
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
